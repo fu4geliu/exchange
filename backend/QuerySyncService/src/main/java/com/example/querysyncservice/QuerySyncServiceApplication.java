57f7e421fc8546ede6be7c362c1bfbcee02c86ba
fu4geliu <3290854198@qq.com>
2026-04-13 15:56:39 +0800

    init

---
+++ backend/QuerySyncService/src/main/java/com/example/querysyncservice/QuerySyncServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.querysyncservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerySyncServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerySyncServiceApplication.class, args);
+    }
+
+}
---
+++ backend/QuerySyncService/src/test/java/com/example/querysyncservice/QuerySyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querysyncservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerySyncServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
